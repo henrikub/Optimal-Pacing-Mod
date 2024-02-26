670be790b31384fd732923fb38d9114c81be69f0
Henrik Brokke <brokkehenrik@gmail.com>
2024-02-26 09:13:10 +0100

    Add the standard files to s4z mods

---
+++ pages/src/wbal.mjs
@@ -0,0 +1,8 @@
+// import * as sauce from '/pages/src/../../shared/sauce/index.mjs';
+// import * as common from '/pages/src/common.mjs';
+
+export async function main() {
+    console.log("test");
+//     let route = common.rpc.getRoute(2737483381);
+//     console.log(route);
+}

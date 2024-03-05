8f3c8b90e6f12acd4b27d647fc6c2ffa8ee50743
Henrik Brokke <brokkehenrik@gmail.com>
2024-03-05 14:43:56 +0100

    Started working on the framework for displaying optimal power

---
+++ pages/src/pacing.mjs
@@ -0,0 +1,106 @@
+import * as sauce from '/pages/src/../../shared/sauce/index.mjs';
+import * as common from '/pages/src/common.mjs';
+
+
+const doc = document.documentElement;
+const L = sauce.locale;
+const H = L.human;
+const num = H.number;
+let imperial = common.storage.get('/imperialUnits');
+L.setImperial(imperial);
+
+let gameConnection;
+
+common.settingsStore.setDefault({
+    autoscroll: true,
+    refreshInterval: 2,
+    overlayMode: false,
+    fontScale: 1,
+    solidBackground: false,
+    backgroundColor: '#00ff00',
+});
+
+
+const spd = (v, entry) => H.pace(v, {precision: 0, suffix: true, html: true, sport: entry.state.sport});
+const pwr = v => H.power(v, {suffix: true, html: true});
+const hr = v => v ? num(v) : '-';
+const dist = v => H.distance(v, {precision: 1, suffix: true, html: true});
+
+
+let overlayMode;
+if (window.isElectron) {
+    overlayMode = !!window.electron.context.spec.overlay;
+    doc.classList.toggle('overlay-mode', overlayMode);
+    document.querySelector('#titlebar').classList.toggle('always-visible', overlayMode !== true);
+    if (common.settingsStore.get('overlayMode') !== overlayMode) {
+        // Sync settings to our actual window state, not going to risk updating the window now
+        common.settingsStore.set('overlayMode', overlayMode);
+    }
+}
+
+function render() {
+
+}
+
+
+
+export async function main() {
+    common.initInteractionListeners();
+    //common.initNationFlags();  // bg okay
+  
+
+    const gcs = await common.rpc.getGameConnectionStatus();
+
+    gameConnection = !!(gcs && gcs.connected);
+    doc.classList.toggle('game-connection', gameConnection);
+    common.subscribe('status', gcs => {
+        gameConnection = gcs.connected;
+        doc.classList.toggle('game-connection', gameConnection);
+    }, {source: 'gameConnection'});
+    common.settingsStore.addEventListener('changed', async ev => {
+        const changed = ev.data.changed;
+        if (changed.has('solidBackground') || changed.has('backgroundColor')) {
+            setBackground();
+        }
+        if (window.isElectron && changed.has('overlayMode')) {
+            await common.rpc.updateWindow(window.electron.context.id,
+                {overlay: changed.get('overlayMode')});
+            await common.rpc.reopenWindow(window.electron.context.id);
+        }
+
+        render();
+        
+    });
+
+    common.storage.addEventListener('globalupdate', ev => {
+        if (ev.data.key === '/imperialUnits') {
+            L.setImperial(imperial = ev.data.value);
+        } 
+    });
+
+    let athleteId;
+    common.subscribe('athlete/watching', watching => {
+        if (watching.athleteId !== athleteId) {
+            athleteId = watching.athleteId;
+        }
+        document.getElementById('current_power').innerHTML = pwr(watching.stats.power.smooth[3])
+    });
+}
+
+function setBackground() {
+    const {solidBackground, backgroundColor} = common.settingsStore.get();
+    doc.classList.toggle('solid-background', !!solidBackground);
+    if (solidBackground) {
+        doc.style.setProperty('--background-color', backgroundColor);
+    } else {
+        doc.style.removeProperty('--background-color');
+    }
+}
+
+
+export async function settingsMain() {
+    common.initInteractionListeners();
+    await common.initSettingsForm('form#general')();
+    //await initWindowsPanel();
+}
+
